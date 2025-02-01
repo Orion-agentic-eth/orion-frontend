ef985dc28376ad2ae916c2694ffe2bc42a88f9a3
Anish Tiwari <87610940+AnishTiwari16@users.noreply.github.com>
2025-02-01 23:38:03 +0530

    Feat: Environment setup + husky setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import viteLogo from '/orion-meta.jpg';
+
+function App() {
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+            </div>
+            <h1>Orion </h1>
+        </>
+    );
+}
+
+export default App;
